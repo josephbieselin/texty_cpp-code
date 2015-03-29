35eeff8daafcc6c707629ca17d9b4f20160f7736
Joseph Bieselin <JoeBieselin@gmail.com>
2015-03-29 00:19:30 -0400

    Attempt at cstr class - Failed

--- testing.cpp
+++ testing.cpp
@@ -15,15 +15,97 @@ using namespace std;
 #define USER_DIR "/files"
 
 
-
 int main() {
-	char* buf = (char*) malloc(MAX_PATH * sizeof(char));
-	char* dir_buf = (char*) malloc(MAX_PATH * sizeof(char));
-	buf = getcwd(buf, MAX_PATH);
-	strcpy(dir_buf, buf);
-	dir_buf = strcat(dir_buf, USER_DIR);
 
 
-	//mkdir(dir_buf, S_IRWXU|S_IRWXG|S_IRWXO);
-	chmod(dir_buf, S_IRWXU|S_IRWXG|S_IRWXO);
+
+
+
+
+
+
+
+
+
+
+	// const char* c = "hello";
+	// my_cstr c2(10, c);
+	// cout << c2 << endl;
+	// c2.new_val("bye");
+	// cout << c2 << endl;
+	// my_cstr c3(c2);
+	// cout << c3 << endl;
+	// c3 = c3;
+	// c3.new_val("hola");
+	// cout << c3 << endl;
+	// c3 = c2;
+	// cout << c3 << endl;
+
+	// my_cstr buf(MAX_PATH);
+	// getcwd(buf.val, MAX_PATH);
+	// // cout << buf.val << endl;
+	// cout << buf.val << endl;
+	// my_cstr dir_buf(buf);
+	// strcat(dir_buf.val, USER_DIR);
+	// cout << dir_buf.val << endl;
+	// cout << buf.val << endl;
+
+	// char* buf = (char*) malloc(MAX_PATH * sizeof(char));
+	// char* dir_buf = (char*) malloc(MAX_PATH * sizeof(char));
+	// buf = getcwd(buf, MAX_PATH);
+	// cout << buf << endl;
+
+
+	// strcpy(dir_buf, buf);
+	// dir_buf = strcat(dir_buf, USER_DIR);
+
+	// const char* filename = "all_users.txt";
+	// ofstream fh;
+	// fh.open(filename);
 }
+
+
+
+
+// // Created for the purpose of the destructor in case a function terminates early, the memory will be freed
+// // Constructor takes in a cstring 
+// class my_cstr {
+// 	friend ostream& operator<<(ostream& os, const my_cstr& this_cstr) {
+// 		return os << this_cstr.val;
+// 	}
+// private:
+// 	int length;
+// public:
+// 	char* val;
+
+// 	// Big 3
+// 	my_cstr(int length, const char* str = "") : val((char*) malloc(length * sizeof(char))), length(strlen(str)) { strcpy(val, str); cout << "Constructor" << endl;}
+// 	my_cstr(const my_cstr& other_cstr) : val((char*) malloc(other_cstr.length * sizeof(char))), length(other_cstr.length) { strcpy(val, other_cstr.val); cout << "Copy constructor" << endl;}
+// 	my_cstr& operator=(const my_cstr& other_cstr) {
+// 		cout << "Assignment operator" << endl;
+// 		if (this == &other_cstr) {
+// 			cout << "Self assignment is weird" << endl;
+// 			return *this;
+// 		}
+// 		free(val);
+// 		length = other_cstr.length;
+// 		val = (char*) malloc(other_cstr.length * sizeof(char));
+// 		strcpy(val, other_cstr.val);
+// 		return *this;
+// 	}
+// 	// Destructor
+// 	~my_cstr() {
+// 		cout << "In the destructor" << endl;
+// 		cout << val << endl;
+// 		free(val);
+// 		cout << val << endl;
+// 	}
+
+// 	// takes a string value to set equal to val
+// 	void new_val(const string& str) {
+// 		cout << "In new_val" << endl;
+// 		free(val);
+// 		val = (char*) malloc(str.length() + 1);
+// 		strcpy(val, str.c_str());
+// 	}
+// };
--- texty_files.cpp
+++ texty_files.cpp
@@ -16,6 +16,7 @@ using namespace std;
 
 #define MAX_PATH 1000		// maximum file path is probably not more than 1000 chars
 #define USER_DIR "/files"	// directory (relative to CWD) where data on all users for texty will be stored
+#define ALL_USERS_FILE "all_users.txt"	// corresponds to file with every user's info and index number
 
 bool is_dir(const char* path)
 {
@@ -35,6 +36,42 @@ bool is_dir(const char* path)
 	return true; // no errors and path was determined to be a directory
 }
 
+// file_exists checks whether the filename passed to it 
+bool file_exists(const char* name)
+{
+	ifstream fh(name);
+	if (fh.good()) {
+		f.close();
+		return true;
+	} else {
+		f.close();
+		return false;
+	}
+}
+
+void create_user_files()
+{
+
+}
+
+// A new user needs to be created
+// Each user has a directory corresponding to their index number
+// Each user's directory has 3 text files: followees, followers, texts
+void create_user_dir(const char* user_index)
+{
+	status = mkdir(user_index, S_IRWXU|S_IRWXG|S_IRWXO);
+	if (status == -1) {
+		// IMPLEMENT WAY TO PASS BACK TO NETWORK THAT ERROR OCCURRED
+		// mkdir was not successful
+		cout << endl << "ERROR: mkdir could not create this new user's directory" << endl;
+		// exit(1); ??????
+		// return -1; ????
+	}
+	chmod(user_index, S_IRWXU|S_IRWXG|S_IRWXO); // give everyone RWX permissions
+
+	create_user_files();
+}
+
 // register takes in 5 strings: first name, last name, email, username, password
 // register stores those strings in a CSV line in all_users.txt in the directory files
 void register(string& fn, string& ln, string& email, string& un, string& pw)
@@ -51,19 +88,48 @@ void register(string& fn, string& ln, string& email, string& un, string& pw)
 		int status;
 		status = mkdir(dir_buf, S_IRWXU|S_IRWXG|S_IRWXO);
 		if (status == -1) {
+			// IMPLEMENT WAY TO PASS BACK TO NETWORK THAT ERROR OCCURRED
 			// mkdir was not successful
-			cout << endl << "ERROR: mkdir could not create directory" << endl;
-			exit(1);
+			cout << endl << "ERROR: mkdir could not create files directory" << endl;
+			// exit(1); ?????
+			// return -1; ???
 		}
 		chmod(dir_buf, S_IRWXU|S_IRWXG|S_IRWXO); // give everyone RWX permissions
 	}
 
 	chdir(dir_buf); // access data inside USER_DIR directory, so change to that directory
+	// Check if username or email are taken, if not taken {add this new user's info}
+	const char* file_path = (char*) malloc(MAX_PATH * sizeof(char));
+	stcpy(file_path, dir_buf);
+	file_path = strcat(file_path, ALL_USERS_FILE);
+	/*
+	Structure of all_users.txt:
+	N
+	username,email,index_i,password,firstname,lastname
+	username,email,index_j,password,firstname,lastname
+	username,email,index_k,password,firstname,lastname
+	... continues on for however many users are in the file
+	Where N is index of the next user (this can go to infinity since numbers are not reused after a user deactivates their account)
+	Each line contains strings representing user info separated by commas and ending with the '\n' character
+	*/
+
+	if (!file_exists(filename)) {
+		// If the file doesn't exist, create it
+		ofstream fh;
+		fh.open(filename);
+		// 1st user created will have an index of 1, which changes N to 2 for the next user to register
+		fh << "2" << '\n';
+		fh << un << "," << email << "," << "1" << "," << pw << "," << fn << "," << ln << '\n';
+		// Create the new user's directory and followees.txt, followers.txt, texts.txt files
+		create_user_dir("1");
+	}
 
 	chdir(buf); // change the CWD back to its initial position at the beginning of the function
+
+	free(buf);
+	free(dir_buf);
+	free(file_path);
 }
-
-
 
 
 int main() {
